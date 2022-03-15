a75095b5acf57fac37dbc85918141f9408913655
Manuel Lera Ramirez <manuel.lera@embl.de>
2022-03-15 11:49:29 +0100

    fixed navbar collapse

--- src/components/MainAppBar.jsx
+++ src/components/MainAppBar.jsx
@@ -4,7 +4,10 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import { AiFillGithub } from 'react-icons/ai';
-import { Button, Tooltip } from '@mui/material';
+import {
+  Button, Tooltip, useMediaQuery, useTheme,
+} from '@mui/material';
+
 import NavbarExportData from './navbar_buttons/NavbarExportData';
 import NavBarLoadData from './navbar_buttons/NavBarLoadData';
 import NavBarEditDescription from './navbar_buttons/NavBarEditDescription';
@@ -14,13 +17,23 @@ function MainAppBar({
   exportData, loadData, showDescription, setShowDescription,
 }) {
   const tooltipText = <div className="tooltip-text">See in GitHub</div>;
+  const theme = useTheme();
+  const wideMode = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
     <AppBar position="static" className="app-bar">
       <div className="app-name">Share Your Cloning</div>
       <Container maxWidth="s">
         <Toolbar disableGutters>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { md: 'flex', xs: 'flex' },
+              flexDirection: { md: 'row', xs: 'column' },
+            }}
+            className={wideMode ? null : 'collapsed'}
+          >
             <NavbarExportData {...{ exportData }} />
             <NavBarLoadData {...{ loadData }} />
             <NavBarEditDescription {...{ showDescription, setShowDescription }} />
