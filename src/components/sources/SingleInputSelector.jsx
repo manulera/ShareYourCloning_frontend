c524c6b7b87038778022ce7d957d4b3c54fe6026
Manuel Lera Ramirez <manulera14@gmail.com>
2024-01-31 16:33:22 +0000

    fixed examples and minor problem with SingleInputSelector

--- src/components/sources/SingleInputSelector.jsx
+++ src/components/sources/SingleInputSelector.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { InputLabel, MenuItem, Select } from '@mui/material';
+import { createSelector } from '@reduxjs/toolkit';
 import { getIdsOfEntitiesWithoutChildSource } from '../../store/cloning_utils';
 
+const customSelector = createSelector(
+  [({ cloning }) => ({ sources: cloning.sources, entities: cloning.entities })],
+  ({ sources, entities }) => getIdsOfEntitiesWithoutChildSource(sources, entities),
+);
+
 function SingleInputSelector({ selectedId, onChange, label, inputEntityIds }) {
-  const options = useSelector(({ cloning }) => getIdsOfEntitiesWithoutChildSource(cloning.sources, cloning.entities));
-  options.push(...inputEntityIds);
+  const idsWithoutChild = useSelector(customSelector);
+  const options = [...idsWithoutChild, ...inputEntityIds];
   return (
     <>
       <InputLabel id="select-single-inputs">{label}</InputLabel>
       <Select
-        value={selectedId}
+        value={selectedId !== null ? selectedId : ''}
         onChange={onChange}
         labelId="select-single-inputs"
         label={label}
