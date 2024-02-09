5b1227fc96518a8aa28a46f4ba7b26244d3f8fa6
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-02-09 12:53:02 +0000

    closes #81 (#93)

--- src/utils/network.js
+++ src/utils/network.js
@@ -13,24 +13,26 @@ function getAllSourceIdsInParentNodes(node) {
   return parentNodesSourceIds.concat(node.parentNodes.flatMap((parentNode) => getAllSourceIdsInParentNodes(parentNode)));
 }
 
+function parentNodeSorter(a, b) {
+  const aValue = getAllSourceIdsInParentNodes(a).concat(a.source.id);
+  const bValue = getAllSourceIdsInParentNodes(b).concat(b.source.id);
+  return Math.min(...aValue) - Math.min(...bValue);
+}
+
 export function constructNetwork(entities, sources) {
-  const shortEntities = entities.map((entity) => ({ ...entity, sequence: '' }));
+  const entitiesNoSeq = entities.map((entity) => ({ ...entity, sequence: '' }));
   const network = [];
   // To construct the network, we start by the elements of DNA that are not input for anything
   // and the sources that have no output
   const entityIdsThatAreInput = sources.reduce((result, source) => result.concat(source.input), []);
-  const entitiesThatAreNotInput = shortEntities.filter((entity) => !entityIdsThatAreInput.includes(entity.id));
+  const entitiesThatAreNotInput = entitiesNoSeq.filter((entity) => !entityIdsThatAreInput.includes(entity.id));
 
   const sourcesWithoutOutput = sources.filter((source) => source.output === null);
 
   entitiesThatAreNotInput.forEach((entity) => network.push({ entity, source: sources.find((s) => s.output === entity.id) }));
   sourcesWithoutOutput.forEach((source) => network.push({ entity: null, source }));
 
-  const unsortedNetwork = network.map((node) => ({ ...node, parentNodes: getParentNodes(node, shortEntities, sources) }));
+  const unsortedNetwork = network.map((node) => ({ ...node, parentNodes: getParentNodes(node, entitiesNoSeq, sources).sort(parentNodeSorter) }));
 
-  return unsortedNetwork.sort((a, b) => {
-    const aValue = getAllSourceIdsInParentNodes(a).concat(a.source.id);
-    const bValue = getAllSourceIdsInParentNodes(b).concat(b.source.id);
-    return Math.min(...aValue) - Math.min(...bValue);
-  });
+  return unsortedNetwork.sort(parentNodeSorter);
 }
