46c561519d2db3942a88ea7a6ecb4fb33216297f
Manuel Lera Ramirez <manulera14@gmail.com>
2024-03-06 17:17:36 +0000

    implemented cypress test and working example

---
+++ cypress/e2e/source_manually_typed.cy.js
@@ -0,0 +1,37 @@
+async function responseGenerator(req) {
+  if (req.body.user_input === 'atata') {
+    req.reply({ statusCode: 200, fixture: 'manually_typed/correct/response_body.json' });
+  } else {
+    throw Error('unhandled input');
+  }
+}
+
+describe('The ManuallyTyped Source', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#tab-panel-0 .MuiInputBase-root').click();
+    cy.get('li[data-value="manually_typed"]').click();
+    cy.intercept('POST', '**/manually_typed', (req) => responseGenerator(req)).as('manuallyTyped');
+  });
+  it('successfully loads', () => {
+    cy.get('#tab-panel-0 #sequence').clear('a');
+    cy.get('#tab-panel-0 #sequence').type('atata');
+    cy.get('.select-source > form > .MuiButtonBase-root').click();
+    cy.wait('@manuallyTyped').its('response.statusCode').should('eq', 200);
+    cy.get('.select-source > :nth-child(2)').should('have.text', 'Manually typed sequence');
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .node-text > .corner-id').should('have.text', '2');
+  });
+
+  it('works only with ACGT', () => {
+    cy.get('.tf-tree').click();
+    cy.get('#tab-panel-0 #sequence').clear('a');
+    cy.get('#tab-panel-0 #sequence').type('ayayay');
+    cy.get('#tab-panel-0 #sequence-helper-text').should('have.text', 'invalid DNA sequence');
+    cy.get('.select-source > form > .MuiButtonBase-root').click();
+    cy.get('#tab-panel-0 #sequence-helper-text').should('have.text', 'invalid DNA sequence');
+    cy.get('#tab-panel-0 #sequence').clear('');
+    cy.get('#tab-panel-0 #sequence-helper-text').should('have.text', 'Field required');
+    cy.get('.select-source > form > .MuiButtonBase-root').click();
+    cy.get('#tab-panel-0 #sequence-helper-text').should('have.text', 'Field required');
+  });
+});
