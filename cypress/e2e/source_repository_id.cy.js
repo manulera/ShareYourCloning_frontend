801b1dac1e46f9d73a9156233f04327b8e34e1d2
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-03-07 12:59:22 +0000

    tests for files + better repository tests

--- cypress/e2e/source_repository_id.cy.js
+++ cypress/e2e/source_repository_id.cy.js
@@ -5,7 +5,7 @@ describe('RepositoryId Source', () => {
     cy.get('li[data-value="repository_id"]').click();
     // cy.intercept('POST', '**/repository_id', (req) => responseGenerator(req)).as('repositoryId');
   });
-  it('works with normal case', () => {
+  it('works with addgene', () => {
     cy.get('body').click();
     cy.get('[aria-labelledby="select-repository-1-label"]').click();
     cy.get('li[data-value="addgene"]').click();
@@ -13,6 +13,25 @@ describe('RepositoryId Source', () => {
     cy.get('#repository-id-1').type('39282');
     cy.get('.select-source > form > .MuiButtonBase-root').click();
     cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .node-text > .corner-id').should('have.text', '2');
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1)').contains('pFA6a');
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1)').contains('5086 bps');
+    cy.get('.select-source > :nth-child(2)').should('have.text', 'Request to addgene with ID 39282 ');
+    // links to https://www.addgene.org/39282/sequences/
+    cy.get('li#source-1 a[href="https://www.addgene.org/39282/sequences/"]').should('be.visible');
+  });
+  it('works with genbank', () => {
+    cy.get('body').click();
+    cy.get('[aria-labelledby="select-repository-1-label"]').click();
+    cy.get('li[data-value="genbank"]').click();
+    cy.get('#repository-id-1').clear('');
+    cy.get('#repository-id-1').type('NM_001018957.2');
+    cy.get('.select-source > form > .MuiButtonBase-root').click();
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .node-text > .corner-id').should('have.text', '2');
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1)').contains('NM_001018957.2');
+    cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1)').contains('2671 bps');
+    cy.get('.select-source > :nth-child(2)').should('have.text', 'Request to genbank with ID NM_001018957.2 ');
+    // links to https://www.ncbi.nlm.nih.gov/nuccore/NM_001018957.2
+    cy.get('li#source-1 a[href="https://www.ncbi.nlm.nih.gov/nuccore/NM_001018957.2"]').should('be.visible');
   });
   it('handles empty submissions and wrong IDs', () => {
     cy.get('body').click();
