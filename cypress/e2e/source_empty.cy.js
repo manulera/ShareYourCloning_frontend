b8864847f424c4604996d8b04f7f3511f84b25f9
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-03-15 16:44:11 +0000

    Fix https://github.com/manulera/ShareYourCloning_frontend/issues/110#issuecomment-1996934760 and better ci gh action

--- cypress/e2e/source_empty.cy.js
+++ cypress/e2e/source_empty.cy.js
@@ -14,8 +14,19 @@ describe('Test empty source functionality', () => {
     cy.get('li#source-1 svg[data-testid="DeleteIcon"]').eq(0).click();
     cy.get('li#source-1').should('not.exist');
     cy.get('li#source-2 svg[data-testid="DeleteIcon"]').eq(0).click();
+    cy.get('li#source-2').should('not.exist');
     cy.get('li#source-3 svg[data-testid="DeleteIcon"]').eq(0).click();
+    cy.get('li#source-3').should('not.exist');
     // There is still a button to create a new one
     cy.get('div.tf-tree svg[data-testid="AddCircleIcon"]').should('exist');
+
+    // Creating new sources resets the counter
+    // (see bug from https://github.com/manulera/ShareYourCloning_frontend/issues/110#issuecomment-1996934760)
+    cy.get('svg[data-testid="AddCircleIcon"]').parent().click();
+    cy.get('svg[data-testid="AddCircleIcon"]').parent().click();
+    cy.get('svg[data-testid="AddCircleIcon"]').parent().click();
+    cy.get('li#source-1').should('exist');
+    cy.get('li#source-2').should('exist');
+    cy.get('li#source-3').should('exist');
   });
 });
