096eeb707c8b617f31f76e4db6310975ab0b7214
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-05-13 11:38:06 +0100

    update deps + general cleanup

--- docker_remake.sh
+++
@@ -1,4 +0,0 @@
-docker container stop frontend_container
-docker container rm frontend_container
-docker build -t frontend_image .
-docker run -d --name frontend_container -p 3000:3000 frontend_image
