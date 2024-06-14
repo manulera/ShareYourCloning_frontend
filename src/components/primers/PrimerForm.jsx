6704403242fa2c34a8d2fa625ce5d44b1911d2aa
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-06-14 16:22:29 +0100

    refactor PrimerForm and PrimerList

--- src/components/primers/PrimerForm.jsx
+++ src/components/primers/PrimerForm.jsx
@@ -8,13 +8,19 @@ import { stringIsNotDNA } from './validators';
 import './PrimerForm.css';
 
 function PrimerForm({
-  primer = { name: '', sequence: '', id: null }, submitPrimer, cancelForm, existingNames, disabledSequenceField,
+  primer = { name: '', sequence: '', id: null }, submitPrimer, cancelForm, existingNames, disabledSequenceText = '',
 }) {
   const [errorStatus, setErrorStatus] = React.useState(primer.id ? { name: false, sequence: false } : { name: true, sequence: true });
   const [touched, setTouched] = React.useState(false);
   const [submissionAttempted, setSubmissionAttempted] = React.useState(false);
   const [name, setName] = React.useState(primer.name);
   const [sequence, setSequence] = React.useState(primer.sequence);
+
+  React.useEffect(() => {
+    setName(primer.name);
+    setSequence(primer.sequence);
+  }, [primer.name, primer.sequence]);
+
   const updateValidationStatus = (fieldName, valid) => {
     setTouched(true);
     // Update the validation status for the given field (taken from the ref.id)
@@ -49,7 +55,7 @@ function PrimerForm({
   const nameErrorChecker = (s) => (existingNames.includes(s) ? { error: true, helperText: 'Name exists' } : { error: false, helperText: '' });
 
   return (
-    <form className="primer-row" onSubmit={onSubmit}>
+    <form className="primer-row" onSubmit={onSubmit} style={{ margin: 'auto' }}>
       <ValidatedTextField
         id="name"
         label="Name"
@@ -57,7 +63,6 @@ function PrimerForm({
         onInputChange={(e) => setName(e.target.value)}
         sx={{ m: 1, display: { width: '20%' } }}
         submissionAttempted={submissionAttempted}
-        defaultValue={primer.name}
         required
         errorChecker={nameErrorChecker}
         updateValidationStatus={updateValidationStatus}
@@ -71,13 +76,12 @@ function PrimerForm({
         sx={{ m: 1, display: { width: '60%' } }}
         className="sequence"
         submissionAttempted={submissionAttempted}
-        defaultValue={primer.sequence}
         required
         errorChecker={sequenceErrorChecker}
         updateValidationStatus={updateValidationStatus}
         floatingHelperText
-        disabled={disabledSequenceField}
-        initialHelperText={disabledSequenceField ? 'Cannot edit sequence in use' : ''}
+        disabled={disabledSequenceText !== ''}
+        initialHelperText={disabledSequenceText}
       />
       {touched
       && (
@@ -87,12 +91,13 @@ function PrimerForm({
         </Tooltip>
       </IconButton>
       )}
-
+      {cancelForm !== null && (
       <IconButton onClick={cancelForm} type="button" sx={{ height: 'fit-content' }}>
         <Tooltip title="Discard changes" arrow placement="top">
           <CancelIcon color="error" />
         </Tooltip>
       </IconButton>
+      )}
 
     </form>
   );
