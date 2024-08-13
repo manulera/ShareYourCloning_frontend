1810a32003fb77f17d2404799d13a868370aeac3
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-08-13 17:04:38 +0200

    closes #226

--- src/components/sources/SourceFile.jsx
+++ src/components/sources/SourceFile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FormHelperText from '@mui/material/FormHelperText';
-import { Checkbox, FormControl, FormControlLabel } from '@mui/material';
+import { Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select } from '@mui/material';
 import SubmitButtonBackendAPI from '../form/SubmitButtonBackendAPI';
 
 // A component providing an interface to import a file
 function SourceFile({ source, requestStatus, sendPostRequest }) {
   const [circularize, setCircularize] = React.useState(false);
+  const [fileFormat, setFileFormat] = React.useState('');
   const onChange = (event) => {
     const files = Array.from(event.target.files);
     const requestData = new FormData();
@@ -14,13 +15,30 @@ function SourceFile({ source, requestStatus, sendPostRequest }) {
       headers: {
         'content-type': 'multipart/form-data',
       },
-      params: { circularize },
+      params: { circularize, sequence_file_format: fileFormat || null },
     };
     sendPostRequest({ endpoint: 'read_from_file', requestData, config, source });
   };
 
   return (
     <form className="submit-sequence-file" onSubmit={(e) => e.preventDefault()}>
+      <FormControl fullWidth>
+        <InputLabel id="select-file-format" shrink>File format</InputLabel>
+        <Select
+          labelId="select-file-format"
+          id="select-file-format"
+          value={fileFormat}
+          onChange={(e) => setFileFormat(e.target.value)}
+          label="File format"
+          displayEmpty
+        >
+          <MenuItem value="">Guess from extension</MenuItem>
+          <MenuItem value="genbank">Genbank</MenuItem>
+          <MenuItem value="fasta">FASTA</MenuItem>
+          <MenuItem value="dna">Snapgene</MenuItem>
+          <MenuItem value="embl">EMBL</MenuItem>
+        </Select>
+      </FormControl>
       <FormControl fullWidth>
         <FormControlLabel control={<Checkbox checked={circularize} onChange={() => setCircularize(!circularize)} />} label="Circularize (FASTA only)" />
       </FormControl>
