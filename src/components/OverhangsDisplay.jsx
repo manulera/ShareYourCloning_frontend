ba9380216b13cbe3537db393ba752bc48bfea1c3
Manuel Lera-Ramirez <manulera14@gmail.com>
2024-10-17 17:36:22 +0100

    fix OverhangsDisplay

--- src/components/OverhangsDisplay.jsx
+++ src/components/OverhangsDisplay.jsx
@@ -3,7 +3,7 @@ import { formatSequenceForOverhangDisplay } from '../utils/sequenceDisplay';
 
 function OverhangsDisplay({ entity, sequenceData }) {
   if (entity === undefined
-    || (sequenceData.overhang_crick_3prime === 0 && sequenceData.overhang_watson_3prime === 0)
+    || (entity.overhang_crick_3prime === 0 && entity.overhang_watson_3prime === 0)
   ) { return null; }
   const { watson, crick, middle } = formatSequenceForOverhangDisplay(
     sequenceData.sequence,
