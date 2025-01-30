b2f1c367b7b851f92496b3d0c85be303c529db62
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-01-30 22:27:15 +0000

    fix hook

--- .husky/no_test_skipping.sh
+++ .husky/no_test_skipping.sh
@@ -2,7 +2,8 @@
 
 echo -e "\033[1;34m>\033[0m Checking for \033[1;33mit.only()\033[0m or \033[1;33mit.skip()\033[0m in test files..."
 
-if grep -r 'it.only(' cypress/e2e/*.cy.js || grep -r 'it.skip(' cypress/e2e/*.cy.js; then
+E2E_TEST_FILES=$(find cypress/e2e -name "*.cy.js")
+if grep -r 'it.only(' $E2E_TEST_FILES || grep -r 'it.skip(' $E2E_TEST_FILES; then
   echo "Error: Found it.only() or it.skip() in test files. Please remove them before committing."
   exit 1
 fi
