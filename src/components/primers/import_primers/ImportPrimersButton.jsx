manulera/ShareYourCloning_frontend

commit ccf72d13577ad972675c96ce605d9eca8b273b89

Import primers from TSV file (#302)
* WIP, several TODOs:
- Reusing code to allow to drop tsv files
- Fix UI
- Check good data format (appropriate header names)
- Decide if wrong inputs can be fixed inplace by user or simply discarded

* Add alerts for invalid, existing or correct oligos

* WIP, adapt alerts after rebase

* Import primers from TSV
- Automatically detect name and sequence headers
- Check if primer names exist or are invalid
- Display primers to be imported in table inside modal
- Use alert hook for errors reading or parsing the file
- Allow users to import valid primers if some are invalid
- Add test tsv files

* small formatting changes

* remove unused css file

* remove unused files + improve import

* simplify styles

* improve rendering logic + status first + column widths + use Mui icons

* refactor parsing logic

* add tests

---------

Co-authored-by: Manuel Lera-Ramirez <manulera14@gmail.com>

src/components/primers/import_primers/ImportPrimersButton.jsx
@@ -1,0 +1,100 @@
+import React, { useState } from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import useAlerts from '../../../hooks/useAlerts';
+import './styles.css';
+
+import { primersFromTsv } from '../../../utils/fileParsers';
+import PrimersImportTable from './ImportPrimersTable';
+
+function ImportPrimersButton({ addPrimer }) {
+  const { addAlert } = useAlerts();
+  const existingNames = useSelector((state) => state.cloning.primers.map((p) => p.name), shallowEqual);
+
+  // Ref to the hidden file input element
+  const hiddenFileInput = React.useRef(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [importedPrimers, setImportedPrimers] = useState([]);
+  const importButtonDisabled = importedPrimers.every((primer) => primer.error !== '');
+
+  // Function to simulate click on hidden file input
+  const handleUploadClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  // Function to handle file selection
+  const handleFileUpload = async (event) => {
+    const fileUploaded = event.target.files[0];
+    try {
+      const uploadedPrimers = await primersFromTsv(fileUploaded, existingNames);
+      setImportedPrimers(uploadedPrimers);
+      setOpenModal(true);
+    } catch (error) {
+      addAlert({
+        message: error.message,
+        severity: 'error',
+      });
+    }
+    event.target.value = null;
+  };
+
+  const handleImportClick = () => {
+    importedPrimers.forEach((primer) => {
+      if (primer.error === '') {
+        addPrimer(primer);
+      }
+    });
+    setOpenModal(false);
+  };
+
+  return (
+    <>
+      <Tooltip arrow title={<span style={{ fontSize: '1.4em' }}>Upload a .tsv file with headers &apos;name&apos; and &apos;sequence&apos;</span>}>
+        <Button
+          onClick={handleUploadClick}
+          variant="contained"
+        >
+          Import from file
+        </Button>
+      </Tooltip>
+
+      <input
+        style={{ display: 'none' }}
+        type="file"
+        ref={hiddenFileInput}
+        onChange={handleFileUpload}
+      />
+
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box className="import-primers-modal-content">
+          <h2 style={{ textAlign: 'center' }}>Import Primers Preview</h2>
+
+          <PrimersImportTable importedPrimers={importedPrimers} />
+
+          <Box className="import-primers-modal-buttons">
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={() => setOpenModal(false)}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={importButtonDisabled}
+              onClick={handleImportClick}
+            >
+              Import Valid Primers
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+export default ImportPrimersButton;

cypress/e2e/primers.cy.js
@@ -231,4 +231,67 @@ describe('Tests primer functionality', () => {
     cy.get('.primer-table-container tr').contains('fwd').should('exist');
     cy.get('.primer-table-container tr').contains('gatctcgccataaaagacag').should('exist');
   });
+  it('Can import primers from tsv file', () => {
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/valid.tsv', { force: true });
+    // There should be a table with the primers displayed
+    cy.get('.import-primers-modal-content').should('exist');
+    cy.get('.import-primers-modal-content tbody tr').should('have.length', 2);
+    cy.get('.import-primers-modal-content [data-testid="CheckCircleIcon"]').should('have.length', 2);
+    cy.get('.import-primers-modal-content tbody tr').eq(0).contains('oligo1');
+    cy.get('.import-primers-modal-content tbody tr').eq(0).contains('cagctagctac');
+    cy.get('.import-primers-modal-content tbody tr').eq(1).contains('oligo2');
+    cy.get('.import-primers-modal-content tbody tr').eq(1).contains('cggttagct');
+    // Clicking on Cancel should close the modal and primers should not be added
+    cy.get('.import-primers-modal-content .MuiButtonBase-root').contains('Cancel').click();
+    cy.get('.import-primers-modal-content').should('not.exist');
+    cy.get('.primer-table-container tr').contains('oligo1').should('not.exist');
+    cy.get('.primer-table-container tr').contains('oligo2').should('not.exist');
+    // Import them
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/valid.tsv', { force: true });
+    cy.get('.import-primers-modal-content button').contains('Import').click();
+    cy.get('.primer-table-container tr').contains('oligo1').should('exist');
+    cy.get('.primer-table-container tr').contains('oligo2').should('exist');
+
+    // Import wrong file should show error
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/wrong_extension.txt', { force: true });
+    cy.get('#global-error-message-wrapper').contains('Headers should have').should('exist');
+    // Close the error
+    cy.get('#global-error-message-wrapper button').click();
+
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/wrong_format.tsv', { force: true });
+    cy.get('#global-error-message-wrapper').contains('All lines should have').should('exist');
+
+    // Can import from file with inverted headers
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/inverted_headers.tsv', { force: true });
+    cy.get('.import-primers-modal-content tr').contains('oligo5').should('exist');
+    cy.get('.import-primers-modal-content tr').contains('oligo6').should('exist');
+    cy.get('.import-primers-modal-content button').contains('Import').click();
+    cy.get('.primer-table-container tr').contains('oligo5').should('exist');
+    cy.get('.primer-table-container tr').contains('oligo6').should('exist');
+
+    // Importing the same ones again should have the add button disabled
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/valid.tsv', { force: true });
+    cy.get('.import-primers-modal-content [data-testid="WarningIcon"]').should('have.length', 2);
+    cy.get('.import-primers-modal-content button').contains('Import').should('be.disabled');
+    cy.get('.import-primers-modal-content button').contains('Cancel').click();
+
+    // It's possible to load a valid and an invalid oligo in the same file
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/valid_and_invalid.tsv', { force: true });
+    cy.get('.import-primers-modal-content [data-testid="CheckCircleIcon"]').should('have.length', 1);
+    cy.get('.import-primers-modal-content [data-testid="CancelIcon"]').should('have.length', 1);
+    cy.get('.import-primers-modal-content button').contains('Import').click();
+    cy.get('.primer-table-container tr').contains('good_oligo2').should('exist');
+
+    // Should fail with csv file
+    cy.get('.primer-form-container').contains('Import from file').click();
+    cy.get('.primer-form-container input').selectFile('cypress/test_files/import_oligos/valid.csv', { force: true });
+    cy.get('#global-error-message-wrapper').contains('Headers should have').should('exist');
+  });
 });

src/components/primers/PrimerList.jsx
@@ -5,6 +5,7 @@ import PrimerForm from './PrimerForm';
 import PrimerTableRow from './PrimerTableRow';
 import './PrimerList.css';
 import { cloningActions } from '../../store/cloning';
+import ImportPrimersButton from './import_primers/ImportPrimersButton';
 
 function getUsedPrimerIds(sources) {
   const forPcr = sources
@@ -83,13 +84,15 @@ function PrimerList() {
           existingNames={primers.map((p) => p.name)}
         />
         )) || (
-        <Button
-          variant="contained"
-          onClick={switchAddingPrimer}
-          size="small"
-        >
-          Add Primer
-        </Button>
+          <div className="primer-add-container">
+            <Button
+              variant="contained"
+              onClick={switchAddingPrimer}
+            >
+              Add Primer
+            </Button>
+            <ImportPrimersButton addPrimer={addPrimer} />
+          </div>
         )}
       </div>
 

src/components/primers/ProgramAddPrimer.jsx
@@ -1,0 +1,31 @@
+import React from 'react';
+import { primersActions } from '../../store/primers';
+import { useDispatch } from 'react-redux';
+
+
+
+function ProgramAddPrimerButton() {
+  const dispatch = useDispatch();
+  const { deletePrimer: deleteAction, addPrimer: addAction, editPrimer: editAction } = primersActions;
+  // const addPrimer = (newPrimer) => dispatch(addAction(newPrimer));
+
+  const handleClick = () => {
+    const newPrimer = {name:"test", sequence:"ATCG"};
+    const clonedPrimer = JSON.parse(JSON.stringify(newPrimer)); // Deep clone
+    dispatch(addAction(newPrimer));
+
+  };
+  
+  // Function to handle file selection
+  const handleChange = (event) => {
+    
+  };
+  
+  return (
+    <div>
+      <button onClick={handleClick}>Add predefined oligo</button>
+    </div>
+  );
+}
+
+export default ProgramAddPrimerButton;

src/components/primers/import_primers/ImportPrimersTable.jsx
@@ -1,0 +1,51 @@
+import React from 'react';
+import { Table, TableCell, TableRow, TableHead, TableBody } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
+import CancelIcon from '@mui/icons-material/Cancel';
+import WarningIcon from '@mui/icons-material/Warning';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+function PrimerTableRow({ primer }) {
+  const { name, sequence, error } = primer;
+
+  let msg = 'Valid primer';
+  let icon = <CheckCircleIcon color="success" />;
+  if (error === 'invalid') {
+    msg = 'Invalid DNA sequence';
+    icon = <CancelIcon color="error" />;
+  } else if (error === 'existing') {
+    msg = 'Primer already exists';
+    icon = <WarningIcon color="warning" />;
+  }
+
+  return (
+    <TableRow>
+      <TableCell align="center">
+        <Tooltip title={msg} placement="top">{icon}</Tooltip>
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell className="sequence-cell">{sequence}</TableCell>
+    </TableRow>
+  );
+}
+
+function PrimersImportTable({ importedPrimers }) {
+  return (
+    <Table className="primers-table">
+      <TableHead>
+        <TableRow>
+          <TableCell align="center">Status</TableCell>
+          <TableCell>Name</TableCell>
+          <TableCell>Sequence</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {importedPrimers.map((primer, index) => (
+          <PrimerTableRow key={index} primer={primer} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+export default PrimersImportTable;

src/components/primers/import_primers/styles.css
@@ -1,0 +1,60 @@
+.import-primers-container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 10px;
+}
+
+.import-primers-modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 80%;
+  max-width: 800px;
+  background-color: #fff;
+  box-shadow: 24;
+  padding: 32px;
+  border-radius: 16px;
+  max-height: 80vh;
+  overflow: auto;
+}
+
+.import-primers-modal-buttons {
+  display: flex;
+  justify-content: flex-end;
+  gap: 1em;
+}
+
+.primers-table {
+  width: 100%;
+  margin-bottom: 24px;
+}
+
+.primers-table th {
+  font-weight: bold;
+  font-size: 1.2em;
+}
+
+/* This makes the two first columns narrow and the sequence column wider */
+.primers-table td:first-child {
+  width: 1%;
+  white-space: nowrap;
+}
+
+.primers-table td:nth-child(2) {
+  width: 1%;
+  white-space: nowrap;
+}
+
+.sequence-cell {
+  font-family: monospace;
+}
+
+.primer-add-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2em;
+}
+

src/index.css
@@ -306,3 +306,32 @@ div#global-error-message-wrapper {
 div#global-error-message-wrapper > div {
     margin: 10px;
 }
+
+
+/* TODO:
+ * Until we find a way to stack notification alerts
+ * we use classes with fixed absolute position
+ */
+
+.notification-alert {
+    position: absolute;
+    top: 1em;
+    left: 1em;
+    z-index: 1000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.notification-alert.import-primer-invalid {
+    top: 5em;
+}
+
+.notification-alert.import-primer-existing {
+    top: 9em;
+}
+
+.notification-alert.import-primer-success {
+    top: 13em;
+}
+

commit e50b530264a69dfd3fc417e4c109905a235c6452

fix error introduced in #302

src/components/primers/import_primers/ImportPrimersButton.jsx
@@ -44,7 +44,7 @@ function ImportPrimersButton({ addPrimer }) {
   const handleImportClick = () => {
     importedPrimers.forEach((primer) => {
       if (primer.error === '') {
-        addPrimer(primer);
+        addPrimer({ name: primer.name, sequence: primer.sequence });
       }
     });
     setOpenModal(false);